6dd2a965cd06d16051702bd175555c79b8e9a41f
umearesn <aakhmetyanov@ozon.ru>
2022-10-16 23:47:01 +0300

    Config fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,7 +19,7 @@ const config: HardhatUserConfig = {
 
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.17",
   networks: {
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
