c2cd78409f21f386ff5a43efdf2554993c0a08e5
umearesn <aakhmetyanov@ozon.ru>
2022-10-16 23:41:44 +0300

    Fillers for api keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "KEY";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "YOUR GOERLI PRIVATE KEY";
+
+require("@nomicfoundation/hardhat-toolbox");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
 };
