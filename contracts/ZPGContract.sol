c0a1129caf4900b1097ee195034f4e9106438037
umearesn <aakhmetyanov@ozon.ru>
2023-03-31 13:43:58 +0300

    фиксы контракта

--- contracts/ZPGContract.sol
+++ contracts/ZPGContract.sol
@@ -16,134 +16,154 @@ contract ZPGContract {
         uint256 lastBattle;
     }
 
+    // CharacterAttributes[] personHeroes;
     mapping(address => CharacterAttributes) public personHeroes;
+    
 
     constructor() {
 
     }
 
-    function createHero(uint seed, string name) public payable {
-        require(personHeroes[address].isValue == false, 'Hero already created');
+    function createHero(uint seed, string calldata name) public payable {
+        address add = payable(msg.sender);
+
+        bool characterExists = ifCharacterExists(add);
+        require(characterExists == false, 'Hero already created');
 
         uint256 balance = msg.value;
-
         uint256 currentPoints = balance / 10000;
 
-        uint256 hp = random(currentPoints, seed);
-        uint256 attack = random(currentPoints - hp, seed);
+        uint256 hp = getRandom(currentPoints, seed) + 1;
+        uint256 attackPoints = getRandom(currentPoints - hp, seed) + 1;
         
-        personHeroes[address] = new CharacterAttributes(
-          name,
-          hp, 
-          hp,
-          attack,
-          balance,
-          block.timestamp);
+        CharacterAttributes memory newHero = CharacterAttributes({
+          name: name,
+          hp: hp, 
+          maxHp: hp,
+          attackDamage: attackPoints,
+          characterBalance: balance,
+          lastBattle: block.timestamp});
+
+        personHeroes[add] = newHero;
     }
 
     function attack(address opponentAddress, uint seed) public {
-        require(personHeroes[address].isValue == true, 'Hero is not created');
-        require(personHeroes[opponentAddress].isValue == true, 'Opponent hero not found');
+        address add = payable(msg.sender);
+
+        CharacterAttributes memory hero = getCharacter(add);
+        CharacterAttributes memory opponent = getCharacter(opponentAddress);
       
-        CharacterAttributes hero = personHeroes[address];
-        uint256 initHp = hero.hp + (hero.lastBattle - block.timestamp) / 60000;
+        uint256 initHp = hero.hp + (block.timestamp - hero.lastBattle) / 60000;
         require(initHp > 0, 'HP is 0, cannot attack');
 
-        CharacterAttributes opponent = personHeroes[opponentAddress];
-        uint256 initOpponnentHp = opponent.hp + (opponent.lastBattle - block.timestamp) / 60000;
-        require(initOpponnentHp > 0, 'Opponent HP is 0, cannot attack');
+        uint256 initOpponentHp = opponent.hp + (block.timestamp - opponent.lastBattle) / 60000;
+        require(initOpponentHp > 0, 'Opponent HP is 0, cannot attack');
 
-        while (initHp > 0 && initOpponnentHp > 0){
-          uint256 turnAttack = (1 + (random(20, seed) - 10) / 100) * hero.attack;
-          initOpponnentHp = max(0, initOpponnentHp - turnAttack);
+        while (initHp > 0 && initOpponentHp > 0){
+          uint256 turnAttack = (1 + (getRandom(20, seed) - 10) / 100) * hero.attackDamage;
+          initOpponentHp = max(0, initOpponentHp - turnAttack);
 
-          uint256 opponentTurnAttack = (1 + (random(20, seed) - 10) / 100) * opponent.attack;
+          uint256 opponentTurnAttack = (1 + (getRandom(20, seed) - 10) / 100) * opponent.attackDamage;
           initHp = max(0, initHp - opponentTurnAttack);
         }
 
         bool playerWon = hero.hp > 0;
 
         if(playerWon == true) {
-            uint256 wonAmount = (opponent.balance / 2);
+            uint256 wonAmount = (opponent.characterBalance / 2);
 
-            personHeroes[address] = new CharacterAttributes(
-              hero.Name,
+            personHeroes[add] = CharacterAttributes(
+              hero.name,
               initHp / 2,
               hero.maxHp,
               hero.attackDamage,
-              hero.balance + wonAmount,
+              hero.characterBalance + wonAmount,
               block.timestamp);
 
-            personHeroes[opponentAddress] = new CharacterAttributes(
-              opponent.Name,
+            personHeroes[opponentAddress] = CharacterAttributes(
+              opponent.name,
               initOpponentHp / 2,
               opponent.maxHp,
               opponent.attackDamage,
-              opponent.balance - wonAmount,
+              opponent.characterBalance - wonAmount,
               block.timestamp);
-        } else if (player1Payout > 0) {
-            uint256 wonAmount = (opponent.balance / 2);
+        } else {
+            uint256 wonAmount = (hero.characterBalance / 2);
 
-
-            personHeroes[opponentAddress] = new CharacterAttributes(
-              opponent.Name,
+            personHeroes[opponentAddress] = CharacterAttributes(
+              opponent.name,
               initOpponentHp / 2,
               opponent.maxHp,
               opponent.attackDamage,
-              opponent.balance + wonAmount,
+              opponent.characterBalance + wonAmount,
               block.timestamp);
 
-            personHeroes[address] = new CharacterAttributes(
-              hero.Name,
+            personHeroes[add] = CharacterAttributes(
+              hero.name,
               initHp / 2,
               hero.maxHp,
               hero.attackDamage,
-              hero.balance - wonAmount,
+              hero.characterBalance - wonAmount,
               block.timestamp);
         }
     }
 
 
     function buff(uint seed) public payable {
-        CharacterAttributes hero = personHeroes[address];
-        require(hero.isValue == true, 'Hero needs to be created first');
+        address add = payable(msg.sender);
 
-        uint256 buffValue = msg.value;
+        CharacterAttributes memory hero = getCharacter(add);
 
-        uint256 currentPoints = balance / 100000;
+        uint256 currentPoints = msg.value / 100000;
 
-        uint256 hpBuff = random(currentPoints, seed);
-        uint256 attackBuff = random(currentPoints, seed);
+        uint256 hpBuff = getRandom(currentPoints, seed);
+        uint256 attackBuff = getRandom(currentPoints, seed);
         
-        personHeroes[address] = new CharacterAttributes(
+        personHeroes[add] = CharacterAttributes(
           hero.name,
           hero.hp + hpBuff, 
           hero.maxHp + hpBuff,
-          hero.attack + attackBuff,
-          balance,
+          hero.attackDamage + attackBuff,
+          hero.characterBalance,
           hero.lastBattle);
     }
 
-    function extract(uint256 extractedAmount) {
+    function extract(uint256 extractedAmount) public {
         // извлечение средств из аккаунта
-        CharacterAttributes hero = personHeroes[address];
+        address add = payable(msg.sender);
+        CharacterAttributes memory hero = getCharacter(add);
 
         require(extractedAmount < hero.characterBalance, "Cannot extract more than on balance");
 
         // Send the payouts
-        (bool success, ) = playerTurns[0].playerAddress.call{value: extractedAmount}("");
+        (bool success, ) = add.call{value: extractedAmount}("");
         require(success, 'Payout failed');
 
-        personHeroes[address] = new CharacterAttributes(
-          hero.Name,
+        personHeroes[add] = CharacterAttributes(
+          hero.name,
           hero.hp,
           hero.maxHp,
           hero.attackDamage,
-          hero.balance -= extract,
+          hero.characterBalance - extractedAmount,
           hero.lastBattle);
     }
 
     function max(uint256 a, uint256 b) private pure returns (uint256) {
         return a >= b ? a : b;
+    }
+    
+    function getRandom(uint256 limit, uint256 seed) private view returns(uint256){
+        return uint(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender, seed))) % limit;
+    }
+
+    function getCharacter(address add) private view returns(CharacterAttributes memory){
+        CharacterAttributes memory hero = personHeroes[add];
+        require(hero.maxHp == 0, 'Hero not found');
+        return hero;
+    }
+
+    function ifCharacterExists(address add) private view returns(bool){
+        CharacterAttributes memory hero = personHeroes[add];
+        return hero.maxHp != 0;
     }
 }
